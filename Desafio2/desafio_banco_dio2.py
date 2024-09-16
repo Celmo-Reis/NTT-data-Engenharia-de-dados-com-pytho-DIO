d9f25311bedd8dd33ebe664660dd90c21f748722
Celmo-Reis <celmohudsonr@gmail.com>
2024-09-16 13:58:35 -0300

    Desafio2 - aprimoramento banco

--- Desafio2/desafio_banco_dio2.py
+++ Desafio2/desafio_banco_dio2.py
@@ -2,6 +2,8 @@ from datetime import datetime
 saldo = 0
 movimentacao = []
 contador_saque = 0
+contador_operacao = 0
+ultima_transacao=0
 
 menu = """
 Digite a operação:
@@ -12,50 +14,66 @@ Digite a operação:
 """
 
 while True:
+    hoje = datetime.today().date()  
 
+    if hoje != ultima_transacao:
+        contador_operacao = 0
+        ultima_transacao = hoje
+        contador_saque = 0
+                
     operacao = input(menu)
-    
+
     if operacao not in ["d", "s", "e", "f"]:
         print("Operação inválida")
     
     elif operacao == "d":
-        valor_deposito = float(input("Qual valor deseja depositar?"))
+
+        if contador_operacao >= 10:
+            print("Limite de transações diárias excedidas")
+
+        else: 
+            valor_deposito = float(input("Qual valor deseja depositar?"))
+    
+            if valor_deposito > 0 : 
+                movimentacao.append(f' +R${valor_deposito:.2f} // {datetime.today().replace(microsecond=0)}')
+                saldo += valor_deposito
+                contador_operacao += 1
+                ultima_transacao = hoje
         
-        if valor_deposito > 0 : 
-            movimentacao.append(f' +R${valor_deposito:.2f} // {datetime.today().replace(microsecond=0)}')
-            saldo += valor_deposito
-            
-        else:
-            print("apenas depositos positivos")
-            
+            else:
+                print("apenas depositos positivos")
+        
     elif operacao == "s":
-        
-        if contador_saque < 3:
+
+        if contador_operacao >= 10:
+            print("Limite de transações diárias excedidas")
+    
+        elif contador_saque < 3:
             valor_saque = float(input("Qual o valor que deseja sacar?"))
-            
+        
             if valor_saque <= saldo:
-                
+            
                 if valor_saque <= 500:
                     movimentacao.append(f' -R${valor_saque:.2f} // {datetime.today().replace(microsecond=0)}')
                     saldo -= valor_saque
                     contador_saque += 1 
-                    
+                    contador_operacao +=1
+                    ultima_transacao = hoje
+                
                 else: 
                     print("NEGADO, limite máximo de R$ 500.00 por saque")      
-                
+            
             else: 
                 print(f"não poderá sacar o dinheiro por falta de saldo {saldo}")
-                
+            
         else:
             print("limite de saque diário esgostado")
-            
+        
     elif operacao == "e":
         print("==============EXTRATO==============\n")
         print("\n".join([f"{valor}\n" for valor in movimentacao]))
         print(f'Saldo: R${saldo:.2f}')
-        print("===================================")     
+        print("===================================")  
 
     elif operacao == "f":
         break           
-
-
---
+++ Desafio2/readme.md
@@ -0,0 +1,5 @@
+# Desafio Dio - Sistema bancário em Python 2
+
+### Objetivo geral - Implementar o desafio1, com as novas funcionalidades:
+
+* **Transações**: Limite de 10 transações diárias. Se o limite for atingido, deve ser informado que ele excedeu o número de transações permitidas para aquele dia. Mostrar no extrato a data e hora de todas as transações.
